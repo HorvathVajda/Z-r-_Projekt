9ad54a5a598ad230cc2b74f2b499944170235b66
Vinars Dániel <VinarsDaniel@pg.local>
2024-11-22 09:15:13 +0100

    1

--- nodeprojekt/routes/bookingRoutes.js
+++ nodeprojekt/routes/bookingRoutes.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const bookingController = require('../controllers/bookingController');
-const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/book', authMiddleware.authenticate, bookingController.bookAppointment);
-=======
 // routes/bookingRoutes.js
 const express = require('express');
 const router = express.Router();
