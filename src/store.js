69726ac6317eb803ea02462b434624480080bd89
Horváth Dániel 1 <HorvathDaniel1@pg.local>
2024-12-03 10:03:16 +0100

    asd

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  isLoggedIn: !!localStorage.getItem("token"),
+  isLoggedIn: !!localStorage.getItem("token"), // Ellenőrzi, hogy van-e token
+
+  // Funkció a bejelentkezési állapot frissítésére
+  logIn(token) {
+    this.isLoggedIn = true;
+    localStorage.setItem("token", token); // Token tárolása
+  },
+
+  logOut() {
+    this.isLoggedIn = false;
+    localStorage.removeItem("token"); // Token törlése
+  },
 });
